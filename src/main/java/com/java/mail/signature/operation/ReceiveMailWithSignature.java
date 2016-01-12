af7692afc879c9e0a84c7aaba28a435795c312cc
zhontao <zhontao@ZHONTAO1.asiapacific.hpqcorp.net>
2016-01-12 11:05:05 +0800

    verify email with signature from Outlook successfully!

--- src/main/java/com/java/mail/signature/operation/ReceiveMailWithSignature.java
+++ src/main/java/com/java/mail/signature/operation/ReceiveMailWithSignature.java
@@ -1,8 +1,7 @@
 package com.java.mail.signature.operation;
 
 import java.security.PrivateKey;
-import java.security.cert.CertSelector;
-import java.security.cert.CertStore;
+import java.security.Security;
 import java.security.cert.X509Certificate;
 import java.util.Collection;
 import java.util.Iterator;
@@ -33,7 +32,6 @@ import org.bouncycastle.cms.RecipientInformation;
 import org.bouncycastle.cms.RecipientInformationStore;
 import org.bouncycastle.cms.SignerInformation;
 import org.bouncycastle.cms.SignerInformationStore;
-import org.bouncycastle.cms.SignerInformationVerifier;
 import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoVerifierBuilder;
 import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient;
 import org.bouncycastle.cms.jcajce.JceKeyTransRecipientId;
@@ -44,54 +42,58 @@ import org.bouncycastle.mail.smime.SMIMEUtil;
 import com.java.mail.util.X509CertUtil;
 
 public class ReceiveMailWithSignature {
+
+	private static final String PROVIDER_NAME = "BC";
+
 	public static void main(String args[]) {
+		ReceiveMailWithSignature receiveMail = new ReceiveMailWithSignature();
+
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 		try {
-			// String issuePfxPath = "ROOTCA.pfx";
-			// String issueCrtPath = "ROOTCA.crt";
-			// String issueAlias = "RootCA";
-			// String issuePassword = "123456";
 			String subjectPfxPath = "ISSUE.pfx";
 			String subjectCrtPath = "ISSUE.crt";
 			String subjectAlias = "HPE";
 			String subjectPassword = "hpe";
 
 			// X509Certificate certificate = X509CertUtil.readX509Certificate("outlook.der.encoded.cer");
-			X509Certificate certificate = X509CertUtil.readX509Certificate("outlook.base64.encoded.cer");
-			// X509Certificate certificate = X509CertUtil.readX509Certificate(subjectCrtPath);
+			// X509Certificate certificate = X509CertUtil.readX509Certificate("outlook.base64.encoded.cer");
+			X509Certificate certificate = X509CertUtil.readX509Certificate(subjectCrtPath);
 			PrivateKey privateKey = X509CertUtil.readPrivateKey(subjectAlias, subjectPfxPath, subjectPassword);
 
 			// Folder folder = getFolder("INBOX");
-			String inbox ="To or Cc me";
+			String inbox = "To or Cc me";
 			Folder folder = getFolder(inbox);
 			// Message messages[] = folder.getMessages();
 
-			// SearchTerm st = new OrTerm(new FromStringTerm("tao.zhong@hpe.com"), new SubjectTerm("Signed Mail Subject"));
-			SearchTerm st = new OrTerm(new FromStringTerm("tao.zhong@hpe.com"), new SubjectTerm("Sign Mail Test"));
+			SearchTerm st = new OrTerm(new FromStringTerm("email address"), new SubjectTerm("Sign Mail Test"));
 			Message[] messages = folder.search(st);
 			int mailCounts = messages.length;
-			System.out.println("Found: " + mailCounts + " mails matched with the condition.");
+			System.out.println("************Found: " + mailCounts + " mails matched with the condition.************");
 
 			if (messages.length == 0) {
 				System.out.println("No Message!");
 			}
 			for (int i = 0; i < messages.length; i++) {
-				System.out.println("the " + (i + 1) + " mail" + "------------------------------------------------");
+				System.out.println("************The " + (i + 1) + " mail" + "************");
 				MimeMessage mail = (MimeMessage) messages[i];
-				String out_from = ((InternetAddress) messages[i].getFrom()[0]).getAddress();
-				System.out.println("From:" + out_from);
-				System.out.println("Subject:" + messages[i].getSubject());
+				String from = ((InternetAddress) messages[i].getFrom()[0]).getAddress();
+				String subject = messages[i].getSubject();
+				System.out.println("From:" + from);
+				System.out.println("Subject:" + subject);
 				System.out.println("Type: " + mail.getContentType());
 				if (mail.isMimeType("multipart/signed")) {
 					System.out.println("a signed mail");
-					receiveSignedMail(mail, certificate);
+					receiveMail.receiveSignedMail(mail);
 				} else if (mail.isMimeType("application/pkcs7-mime") || mail.isMimeType("application/x-pkcs7-mime")) {
 					System.out.println("a enveloped mail");
-					receiveEnveloped(mail, privateKey, certificate);
+					receiveMail.receiveEnveloped(mail, privateKey, certificate);
 				} else {
 					System.out.println("not a identified mail");
 				}
-
+				/* Delete an email */
 				// message[i].setFlag(Flags.Flag.DELETED, true);
+				/* Move an email to specific box */
+				// TODO
 			}
 			folder.close(true);
 		} catch (Exception e) {
@@ -99,8 +101,7 @@ public class ReceiveMailWithSignature {
 		}
 	}
 
-	public static void receiveEnveloped(Message mail, PrivateKey key, X509Certificate cert) {
-
+	public Object receiveEnveloped(Message mail, PrivateKey key, X509Certificate cert) {
 		try {
 			SMIMEEnveloped enveloped = new SMIMEEnveloped((MimeMessage) mail);
 
@@ -119,9 +120,9 @@ public class ReceiveMailWithSignature {
 					if (mp.getCount() > 1)
 						index = 1;
 					Part tmp = mp.getBodyPart(index);
-					System.out.println("Content: " + tmp.getContent());
+					return tmp.getContent();
 				} else {
-					System.out.println("Content: " + recoveredPart.getContent());
+					return recoveredPart.getContent();
 				}
 			} else {
 				System.out.println("decrypt error");
@@ -129,62 +130,33 @@ public class ReceiveMailWithSignature {
 		} catch (Exception e) {
 			e.printStackTrace(System.out);
 		}
+		return null;
 	}
 
-	public static void receiveSignedMail(Message msg, X509Certificate cert) throws MessagingException {
-		
+	public MimeBodyPart receiveSignedMail(Message msg) throws MessagingException {
 		try {
-
-			MimeMessage newmsg = new MimeMessage((MimeMessage) msg);
-//			newmsg.setHeader("micalg", "multipart/signed; protocol=application/x-pkcs7-signature; micalg=2.16.840.1.101.3.4.2.1;boundary="----=_NextPart_000_0109_01D1496F.83293B50"");
-			newmsg.saveChanges();
-			System.out.println("newmsg ContentType(): " + newmsg.getContentType());
-			// SMIMESigned signed = new SMIMESigned((MimeMultipart) newmsg.getContent());
-			SMIMESigned signed = new SMIMESigned((MimeMultipart) newmsg.getContent());
-
-			////
 			/*
-			 * SignerInformationStore signers = signed.getSignerInfos();
+			 * Add a header to make a new message in order to fix the issue of Outlook
 			 * 
-			 * org.bouncycastle.util.Store certs = (org.bouncycastle.util.Store)signed.getCertificates();
+			 * @see http://www.bouncycastle.org/wiki/display/JA1/CMS+and+SMIME+APIs
 			 * 
-			 * Collection c = signers.getSigners();
-			 * Iterator it = c.iterator();
-			 * while (it.hasNext()) {
-			 * SignerInformation signer = (SignerInformation) it.next();
-			 * Collection certCollection = ((org.bouncycastle.util.Store) certs).getMatches(signer.getSID());
-			 * 
-			 * Iterator certIt = certCollection.iterator();
-			 * X509CertificateHolder certHolder = (X509CertificateHolder) certIt.next();
-			 * X509Certificate certificate = new JcaX509CertificateConverter().setProvider("BC").getCertificate(certHolder);
-			 * 
-			 * if (signer.verify((SignerInformationVerifier) certificate.getPublicKey())) {
-			 * // verified++;
-			 * System.out.println("verification succeeded");
-			 * }else {
-			 * System.out.println("verification failed");
-			 * }
-			 * }
+			 * @see http://stackoverflow.com/questions/8590426/s-mime-verification-with-x509-certificate
 			 */
-			// org.bouncycastle.util.Store store = (org.bouncycastle.util.Store) signedData.getCertificates();
-			// Collection certCollection = ((org.bouncycastle.util.Store) store).getMatches(signer.getSID());
-			// Iterator certIt = certCollection.iterator();
-			// X509CertificateHolder certHolder = (X509CertificateHolder) certIt.next();
-			// X509Certificate certificate = new JcaX509CertificateConverter().setProvider("BC").getCertificate(certHolder);
-			///
-
-			if (isValid(signed, cert)) {
+			MimeMessage newmsg = new MimeMessage((MimeMessage) msg);
+			newmsg.setHeader("Add a header for verifying signature only.", "nothing");
+			newmsg.saveChanges();
+			SMIMESigned signed = new SMIMESigned((MimeMultipart) newmsg.getContent());
+			if (isValid(signed)) {
 				System.out.println("verification succeeded");
 			} else {
 				System.out.println("verification failed");
 			}
-			signed = new SMIMESigned((MimeMultipart) msg.getContent());
 			MimeBodyPart content = signed.getContent();
-
-			System.out.println("Content: " + content.getContent());
+			return content;
 		} catch (Exception e) {
 			e.printStackTrace(System.out);
 		}
+		return null;
 	}
 
 	public static Folder getFolder(String folderName) {
@@ -195,8 +167,8 @@ public class ReceiveMailWithSignature {
 			/* To support SSL, using protocl 'pop3s' or 'imaps' */
 			String port = "993";
 			String receivingProtocol = "imaps";
-			String username = "tao.zhong@hpe.com";
-			String password = "Cisco01!";
+			String username = "your email address";
+			String password = "your password";
 			boolean proxySet = true;
 			String proxyHost = "172.28.2.1";
 			String proxyPort = "85";
@@ -215,12 +187,18 @@ public class ReceiveMailWithSignature {
 			props.put("mail.smtp.port", port);
 			props.put("mail.smtp.auth", auth);
 			props.put("mail.store.protocol", receivingProtocol);
-			//Proxy
+			// Proxy
 			props.put("proxySet", proxySet);
-			props.put("http.proxyHost",proxyHost); 
-			props.put("http.proxyPort",proxyPort); 
-//			props.put("socksProxyHost", "192.168.1.1");
-//			props.put("socksProxyPort", "1080");
+			props.put("http.proxyHost", proxyHost);
+			props.put("http.proxyPort", proxyPort);
+			// props.put("socksProxyHost", "192.168.1.1");
+			// props.put("socksProxyPort", "1080");
+			/*
+			 * General Issues with Multiparts
+			 * 
+			 * @see http://www.bouncycastle.org/wiki/display/JA1/CMS+and+SMIME+APIs
+			 */
+			// props.put("mail.mime.cachemultipart", false);
 			Session session = Session.getDefaultInstance(props, null);
 			Store store = session.getStore(receivingProtocol);
 			store.connect(host, username, password);
@@ -233,22 +211,27 @@ public class ReceiveMailWithSignature {
 		}
 	}
 
-	public static boolean isValid(CMSSignedData signedData, X509Certificate cert) {
+	public static boolean isValid(CMSSignedData signedData) {
 		try {
 			SignerInformationStore signers = signedData.getSignerInfos();
 			Iterator<SignerInformation> it = signers.getSigners().iterator();
-
-			if (it.hasNext()) {
+			boolean verify = false;
+			while (it.hasNext()) {
 				SignerInformation signer = it.next();
-				// org.bouncycastle.util.Store store = (org.bouncycastle.util.Store) signedData.getCertificates();
-				// Collection certCollection = ((org.bouncycastle.util.Store) store).getMatches(signer.getSID());
-				// Iterator certIt = certCollection.iterator();
-				// X509CertificateHolder certHolder = (X509CertificateHolder) certIt.next();
-				// X509Certificate certificate = new JcaX509CertificateConverter().setProvider("BC").getCertificate(certHolder);
-				return signer.verify(new JcaSimpleSignerInfoVerifierBuilder().setProvider("BC").build(cert));
+				org.bouncycastle.util.Store store = (org.bouncycastle.util.Store) signedData.getCertificates();
+				Collection certCollection = ((org.bouncycastle.util.Store) store).getMatches(signer.getSID());
+				Iterator certIt = certCollection.iterator();
+				X509CertificateHolder certHolder = (X509CertificateHolder) certIt.next();
+				X509Certificate certificate = new JcaX509CertificateConverter().setProvider(PROVIDER_NAME).getCertificate(certHolder);
+				verify = signer.verify(new JcaSimpleSignerInfoVerifierBuilder().setProvider(PROVIDER_NAME).build(certificate));
+				/*
+				 * SignerInformation signer = it.next();
+				 * // String certPath = "ISSUE.crt";
+				 * String certPath = "outlook.base64.encoded.cer";
+				 * verify = signer.verify(new JcaSimpleSignerInfoVerifierBuilder().setProvider("BC").build(X509CertUtil.readX509Certificate(certPath)));
+				 */
 			}
-
-			return false;
+			return verify;
 		} catch (Exception e) {
 			e.printStackTrace(System.out);
 			return false;
